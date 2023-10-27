5bdb2bb5f8b68cd0c0f52b91c603074a6736e41b
felix-ha <hauerf98@gmail.com>
2023-10-27 21:58:41 +0200

    Remove mock model and process result

--- Hello.py
+++ Hello.py
@@ -9,7 +9,6 @@ ip_adress_server = os.getenv('IP_ADRESS_SERVER', 'localhost')
 port = 5000
 url_server = f'http://{ip_adress_server}:{port}'
 route_check = '/up-status'
-route_mock_model = '/mock-llm'
 route_model = '/llm'
 
 
@@ -28,11 +27,6 @@ def ask_model(url_server: str, route_model: str, question: str, context: str):
     headers = {'content-type': 'application/json'}
     response = requests.post(f'{url_server}/{route_model}', data=json.dumps(payload), headers=headers)
     return response.json()
-
-def mock_ask_model(question: str, context: str):
-    time.sleep(1)
-    return{'inference_time_seconds': 1.5181848, 'answer': "Das weiß ich nicht."}
-
 
 
 st.write("""
@@ -75,7 +69,8 @@ if st.button('Stelle Frage'):
     if server_is_online(url_server, route_check):
       if question != "":
           with st.spinner('LLM denkt...'):
-              result = ask_model(url_server, route_mock_model, question, context)
-              st.text_area(f'Antwort (dauerte {result["inference_time_seconds"]:.2f} Sekunden)', result['answer'])
+              result = ask_model(url_server, route_model, question, context)
+              answer = result['answer'][0]['generated_text'].split('<|im_start|>assistant')[1]
+              st.text_area(f'Antwort (dauerte {result["inference_time_seconds"]:.2f} Sekunden)', answer)
     else:
       st.warning('LLM ist offline, keine Antwort möglich!', icon="⚠️")
